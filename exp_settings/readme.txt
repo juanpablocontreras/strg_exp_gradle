5203b84dcd895996afb18b7e457497539e781185
juancontreras <juanpablocontreras@cmail.carleton.ca>
2020-08-24 10:57:30 -0400

    created readme for settings, changed handler setting to read max type

---
+++ exp_settings/readme.txt
@@ -0,0 +1,25 @@
+README
+
+Each settings file holds the values that will be read by the corresponding _setter java file. Settings are separated by a new line.
+
+Controller:
+maxQueueSize
+logIdentifier
+
+Creator:
+connectionStr
+username
+password
+table_name
+total_number_of_items
+
+Handler:
+number of IO requests per data transfer
+inter io processing time
+max type (size or number of IO requests)
+
+Transmitter:
+connectionStr
+username
+password
+table_name
--- src/main/java/experiment/Handler_setter.java
+++ src/main/java/experiment/Handler_setter.java
@@ -4,11 +4,13 @@ package experiment;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import global_enums.Handler_Max_Type;
 
 
 public class Handler_setter {
 	public int number_of_IO_requests_per_data_transfer;
 	public int inter_IO_processing_time;
+	public Handler_Max_Type max_type;
 	
 	
 	public Handler_setter() {
@@ -22,6 +24,7 @@ public class Handler_setter {
 			
 			this.number_of_IO_requests_per_data_transfer = Integer.parseInt(myReader.nextLine());
 			this.inter_IO_processing_time = Integer.parseInt(myReader.nextLine());
+			this.max_type = Handler_Max_Type.valueOf(myReader.nextLine());
 			
 			myReader.close();
 			
